25bb0a57e593839a2a415272d8a967c871dfc4c7
AlphaXdream <1260840442@qq.com>
2023-03-11 10:42:28 +0800

    keybindings.lua

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -19,20 +19,22 @@ if not InVsCode() then
     map("n", "<A-j>", "<C-w>j", opt)
     map("n", "<A-k>", "<C-w>k", opt)
     map("n", "<A-l>", "<C-w>l", opt)
-
-    -- bufferline
-    map('n', "<C-h>", ":BufferLineCyclePrev<CR>", opt)
-    map('n', "<C-l>", ":BufferLineCycleNext<CR>", opt)
 else
     map('n', '<leader>b', ':call VSCodeNotify(\"workbench.action.toggleSidebarVisibility\")<CR>', opt)
     map('n', '<A-m>', ':call VSCodeNotify(\"workbench.view.explorer\")<CR>', opt)
 end
+
+-- tab
+map('n', "<C-h>", ":tabpreview<CR>", opt)
+map('n', "<C-l>", ":tabnext<CR>", opt)
 
 -- normal mode
 map('n', '<CR>', 'o<ESC>', opt)
 map('n', '<S-CR>', 'O<ESC>', opt)
 map('n', 'H', '^', opt)
 map('n', 'L', '$', opt)
+map('v', 'H', '^', opt)
+map('v', 'L', '$', opt)
 map('n', 'J', '10j', opt)
 map('n', 'K', '10k', opt)
-map('n', '<leader>at', 'ggVG', opt) -- 选中所有文本
+map('n', '<leader>at', 'ggVG', opt) -- 选中所有文本 all text
